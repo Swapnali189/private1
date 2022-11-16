0bf396c14aeb952c9e9a95a3183e02bf78b24fc6
Swapnali189 <118351559+Swapnali189@users.noreply.github.com>
2022-11-16 16:39:15 +0530

    Update exceldata.java

--- seleniumpractice/src/automation/exceldata.java
+++ seleniumpractice/src/automation/exceldata.java
@@ -11,6 +11,9 @@ public class exceldata {
 		
 		String data=WorkbookFactory.create(file).getSheet("Sheet1").getRow(0).getCell(0).getStringCellValue();
 		System.out.println(data);
+		System.out.println(data);
+		System.out.println(data);
+		System.out.println(data);
 		
 	}
 }
